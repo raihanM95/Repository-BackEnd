1dc8954c0aeb9b5261aaa06e663af2b85e744d01
SH_SHuvo <khaledul.islam@hotmail.com>
2021-09-13 18:46:48 +0600

    adding  base model

---
+++ MSDSL_Utilities/Common/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDSL_Utilities.Common
+{
+    public class BaseModel
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public int Version { get; set; }
+    }
+}
